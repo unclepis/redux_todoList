c61035f83d1d751ab8c1d55900e36617dd2fc044
unclepis <59869158@qq.com>
2018-03-01 17:10:53 +0800

    Create TodoList.jsx

---
+++ src/containers/TodoList.jsx
@@ -0,0 +1,37 @@
+import { connect } from 'react-redux'
+import TodoList from '../components/TodoList'
+import { toggleItem, deleteItem } from '../actions/actions'
+
+const filterList = (todos, filter) => {
+    switch (filter) {
+        case 'All':
+            return todos
+        case 'Active':
+            return todos.filter(todo => !todo.completed)
+        case 'Completed':
+            return todos.filter(todo => todo.completed)
+        default:
+            return todos
+    }
+}
+const mapStateToProps = state => {
+    return {
+        todos: filterList(state.todos, state.filter)
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onDelete: (id) => {
+            dispatch(deleteItem(id))
+        },
+        onToggle: (id) => {
+            dispatch(toggleItem(id))
+        }
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(TodoList)
