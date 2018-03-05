2328e8082377fd02f458d6fea0d6962e9f8415c4
unclepis <59869158@qq.com>
2018-03-05 17:24:10 +0800

    Update filter.js

--- src/reducers/filter.js
+++ src/reducers/filter.js
@@ -1,7 +1,12 @@
 export const filter = (state = 'All', action) => {
     switch (action.type) {
         case 'set_Filter':
-            return action.filter
+            return action.payload
+        case 'load_item':
+            if (JSON.parse(localStorage.getItem('state'))) {
+                return JSON.parse(localStorage.getItem('state')).filter
+            }
+            return state
         default:
             return state
     }
