bd72fd67c957039a20b8e38584404b98e05f131e
unclepis <59869158@qq.com>
2018-03-05 17:14:49 +0800

    Update TodoList.jsx
    读取localStorage

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,18 +1,37 @@
 import React from 'react'
 import Todo from './Todo'
 
-const TodoList = ({ todos, onToggle, onDelete }) => {
-    return (
-        <ul style={{ width: '200px', minHeight: '200px', border: '1px dashed' }}>
-            {
-                todos.map((todo, index) => {
-                    return (
-                        <Todo key={index} {...todo} onToggle={() => onToggle(todo.id)} onDelete={() => onDelete(todo.id)} />
-                    )
+export default class TodoList extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            loading: true
+        }
+    }
+    componentWillMount() {
+        if (this.props.onLoad) {
+            setTimeout(() => {
+                this.setState({
+                    loading: false
                 })
-            }
-        </ul>
-    )
+                this.props.onLoad();
+            }, 2000)
+        }
+    }
 
+    render() {
+        const { todos, onToggle, onDelete } = this.props;
+        return (
+            <ul style={{ width: '200px', minHeight: '200px', border: '1px dashed' }}>
+                {
+                    this.state.loading ? <h1 style={{ display: 'block', width: '40px', height: '40px'}}>Loading...</h1> :
+                        todos.map((todo, index) => {
+                            return (
+                                <Todo key={index} {...todo} onToggle={() => onToggle(todo.id)} onDelete={() => onDelete(todo.id)} />
+                            )
+                        })
+                }
+            </ul>
+        )
+    }
 }
-export default TodoList
